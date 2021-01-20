3536911197d4fe52a915085269b3cfef335e3d4c
Ahmed <qahmed89@gmail.com>
2021-01-20 18:40:35 +0200

    Part 4 Dices App

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,57 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  _DicePage createState() => _DicePage();
+}
+
+class _DicePage extends State {
+  var leftDice = 1;
+  var rightDice = 1;
+  void _RandomNumber() {
+    setState(() {
+      leftDice = Random.secure().nextInt(6) + 1;
+      rightDice = Random.secure().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        child: Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              child: Image.asset("images/dice$leftDice.png"),
+              onPressed: () {
+                _RandomNumber();
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset(
+                "images/dice$rightDice.png",
+              ),
+              onPressed: () {
+                _RandomNumber();
+              },
+            ),
+          ),
+        ],
+      ),
+    ));
   }
+}
+
+void _showToast(BuildContext context) {
+  final scaffold = Scaffold.of(context);
+  scaffold.showSnackBar(
+    SnackBar(
+      content: const Text('Added to favorite'),
+      action: SnackBarAction(
+          label: 'UNDO', onPressed: scaffold.hideCurrentSnackBar),
+    ),
+  );
 }
